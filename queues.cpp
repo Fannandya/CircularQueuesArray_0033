88316dbbb52a92bb60e0b3b4870b853652c1c531
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-06-12 10:58:43 +0700

    add switch dan catch bagian dari try

--- queues.cpp
+++ queues.cpp
@@ -139,5 +139,43 @@ int main()
             cout << "enter your choice (1-4): " << endl;
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+
+            case '3':
+            {
+                q.display();
+                break;
+            }
+
+            case '4':
+            {
+                return 0;
+            }
+
+            default:
+            {
+                cout << "invalid option!" << endl;
+                break;
+            }
+            }
         }
-    };
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+    }
+    return 0;
+};
