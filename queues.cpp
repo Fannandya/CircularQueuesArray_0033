8951ae983ed60b9144a70c123acf0829b391ce10
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-06-12 10:12:36 +0700

    mengembalikan array ke awal jika posisi rear di akhir array

--- queues.cpp
+++ queues.cpp
@@ -33,5 +33,11 @@ public:
             front = 0;
             rear = 0;
         }
+        else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (rear == max - 1)
+                rear = 0;
         }
+    }
 };
